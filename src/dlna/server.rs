9409e80843a45c11412a9bd6e138d4f6ccbab132
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-10 22:26:34 +0800

    move stream

--- src/dlna/server.rs
+++ src/dlna/server.rs
@@ -1,20 +1,14 @@
 use axum::{
     Router,
-    extract::Path,
-    http::StatusCode,
-    http::Request,
-    body::Body,
-    response::IntoResponse,
     routing::{get, post},
 };
 use std::net::SocketAddr;
 use std::sync::Arc;
 use tokio::net::TcpListener;
-use tower_http::services::ServeFile;
 use super::root::handle_root_desc;
 use super::connection_manager::{handle_connection_manager, handle_ctl_connection_manager};
 use super::content_directory::{handle_content_directory, handle_ctl_content_directory};
-
+use super::stream::handle_stream;
 
 #[derive(Clone)]
 pub struct DlnaState {
@@ -59,18 +53,3 @@ impl DlnaServer {
         Ok(())
     }
 }
-
-pub async fn handle_stream(
-    Path(path): Path<String>,
-    req: Request<Body>,
-) -> impl IntoResponse {
-    // Axum strips the leading slash from wildcard matches, so re-add it
-    let full_path = format!("/{}", path);
-    println!("[INFO] Serving file: {}", full_path);
-
-    match ServeFile::new(&full_path).try_call(req).await {
-        Ok(response) => response.into_response(),
-        Err(_) => (StatusCode::INTERNAL_SERVER_ERROR, "Failed to stream file").into_response(),
-    }
-}
-
