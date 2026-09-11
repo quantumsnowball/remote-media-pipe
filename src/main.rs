6e6f0eed5a723a74114e607664e9638d5ebc8b64
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-11 15:41:56 +0800

    print_ssh_config

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,7 @@ use clap::{Subcommand};
 use std::path::PathBuf;
 use provider::MediaSource;
 use provider::local::LocalSource;
-use provider::sftp::read_ssh_config;
+use provider::sftp::print_ssh_config;
 
 #[derive(Parser, Debug)]
 #[command(name = "remote-media-pipe")]
@@ -50,14 +50,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             (Arc::new(LocalSource::new(source)), target)
         }
         ProviderSubcommand::Sftp { source, target } => {
-            let c = read_ssh_config(&source)?;
-            println!("--- SSH CONFIG RESOLUTION ---");
-            println!(" host name     : {}", c.addr);
-            println!(" port          : {}", c.port);
-            println!(" user          : {}", c.user);
-            println!(" identity file : {:?}", c.identity_file);
-            println!(" remote path   : {}", c.remote_path);
-            println!("-----------------------------");
+            print_ssh_config(&source)?;
             println!(" target        : {}", target);
             todo!("Implement sftp");
         }
