293817ca0a360ea37394ff263956e5cd54a3c708
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-17 23:33:17 +0800

    refactor: move path_id cache logic into path_id.rs new module

--- src/provider/gdrive/mod.rs
+++ src/provider/gdrive/mod.rs
@@ -1,6 +1,7 @@
 mod auth;
 mod config;
 mod file_list;
+mod path_id;
 mod source;
 
 pub use config::print_gdrive_config;
--- src/provider/gdrive/source.rs
+++ src/provider/gdrive/source.rs
@@ -1,22 +1,19 @@
 use super::auth::get_valid_access_token;
 use super::config::GDriveHostInfo;
-use super::file_list::{FileListCache, FileListResponse};
+use super::file_list::FileListCache;
+use super::path_id::PathResolver;
 use crate::provider::{MediaEntry, MediaSource};
 use async_trait::async_trait;
 use axum::body::Body;
 use axum::http::Response;
 use reqwest::header::{AUTHORIZATION, CONTENT_LENGTH, CONTENT_RANGE, CONTENT_TYPE, RANGE};
-use std::collections::HashMap;
 use std::io;
-use std::sync::Arc;
-use tokio::sync::RwLock;
 
 pub struct GDriveSource {
     pub host_info: GDriveHostInfo,
     pub client: reqwest::Client,
     root_path: String,
-    // in-memory path-to-id cache
-    path_cache: Arc<RwLock<HashMap<String, (String, bool)>>>,
+    path_resolver: PathResolver,
     file_list_cache: FileListCache,
 }
 
@@ -27,107 +24,9 @@ impl GDriveSource {
             host_info, //
             client: reqwest::Client::new(),
             root_path,
-            path_cache: Arc::new(RwLock::new(HashMap::new())),
+            path_resolver: PathResolver::new(),
             file_list_cache: FileListCache::new(),
         }
-    }
-
-    // resolve virtual path like folder1/file.mp4 to drive file/folder id
-    async fn resolve_path_to_id(
-        &self, //
-        access_token: &str,
-        path: &str,
-    ) -> io::Result<(String, bool)> {
-        // prepend self.root_path if path is empty or relative
-        let full_path = match (self.root_path.trim_matches('/'), path.trim_matches('/')) {
-            ("", "") => String::new(),
-            ("", p) => p.to_string(),
-            (r, "") => r.to_string(),
-            (r, p) => format!("{}/{}", r, p),
-        };
-
-        if full_path.is_empty() {
-            return Ok(("root".to_string(), true));
-        }
-
-        // fast path: read lock check
-        {
-            let cache = self.path_cache.read().await;
-            if let Some(entry) = cache.get(&full_path) {
-                return Ok(entry.clone());
-            }
-        }
-
-        // resolve path level by level via API calls
-        let segments: Vec<&str> = full_path.split('/').collect();
-        let mut current_id = "root".to_string();
-        let mut is_dir = true;
-        let mut accumulated_path = String::new();
-
-        for segment in segments {
-            if !accumulated_path.is_empty() {
-                accumulated_path.push('/');
-            }
-            accumulated_path.push_str(segment);
-
-            // check if subpath is already cached
-            {
-                let cache = self.path_cache.read().await;
-                if let Some((cached_id, cached_is_dir)) = cache.get(&accumulated_path) {
-                    current_id = cached_id.clone();
-                    is_dir = *cached_is_dir;
-                    continue;
-                }
-            }
-
-            // query drive api for folder/file/shortcut id
-            let query = format!(
-                "'{}' in parents and name = '{}' and trashed = false",
-                current_id,
-                segment.replace('\'', "\\'")
-            );
-
-            let res: FileListResponse = self
-                .client
-                .get("https://www.googleapis.com/drive/v3/files")
-                .header(AUTHORIZATION, format!("Bearer {}", access_token))
-                .query(&[
-                    ("q", query.as_str()), //
-                    ("fields", "files(id, name, mimeType, shortcutDetails)"),
-                ])
-                .send()
-                .await
-                .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?
-                .json()
-                .await
-                .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
-
-            let file = res.files.first().ok_or_else(|| {
-                io::Error::new(io::ErrorKind::NotFound, format!("path component '{}' not found", segment))
-            })?;
-
-            // if entry is a shortcut, extract target_id and target_mime_type
-            let (real_id, real_mime) = if file.mime_type == "application/vnd.google-apps.shortcut" {
-                if let Some(ref details) = file.shortcut_details {
-                    let tid = details.target_id.clone().unwrap_or_else(|| file.id.clone());
-                    let tmime = details.target_mime_type.clone().unwrap_or_else(|| file.mime_type.clone());
-                    (tid, tmime)
-                } else {
-                    (file.id.clone(), file.mime_type.clone())
-                }
-            } else {
-                (file.id.clone(), file.mime_type.clone())
-            };
-
-            current_id = real_id;
-            is_dir = real_mime == "application/vnd.google-apps.folder";
-
-            // store resolved target path in cache
-            let mut cache = self.path_cache.write().await;
-            cache.insert(accumulated_path.clone(), (current_id.clone(), is_dir));
-        }
-
-        Ok((current_id, is_dir))
     }
 }
 
@@ -138,7 +37,7 @@ impl MediaSource for GDriveSource {
             .await
             .map_err(|e| io::Error::new(io::ErrorKind::PermissionDenied, e))?;
 
-        let (folder_id, is_dir) = self.resolve_path_to_id(&token, path).await?;
+        let (folder_id, is_dir) = self.path_resolver.resolve(&self.client, &token, &self.root_path, &path).await?;
         if !is_dir {
             return Err(io::Error::new(io::ErrorKind::InvalidInput, "path is not a directory"));
         }
@@ -180,7 +79,7 @@ impl MediaSource for GDriveSource {
             .await
             .map_err(|e| io::Error::new(io::ErrorKind::PermissionDenied, e))?;
 
-        let (file_id, is_dir) = self.resolve_path_to_id(&token, path).await?;
+        let (file_id, is_dir) = self.path_resolver.resolve(&self.client, &token, &self.root_path, &path).await?;
         if is_dir {
             return Err(io::Error::new(io::ErrorKind::InvalidInput, "cannot stream a directory"));
         }
