18b5483b32e8a1a922d816e566b9380cfefe2e38
Quantum Snowball <quantum.snowball@gmail.com>
2026-09-17 18:21:40 +0800

    args module

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,16 @@
+mod args;
 mod dlna;
 mod logging;
 mod provider;
 mod ssdp;
-use clap::Parser;
-use clap::Subcommand;
+use args::parse_and_resolve_args;
 use dlna::DlnaServer;
 use logging::init_logging;
-use provider::MediaSource;
-use provider::gdrive::GDriveSource;
-use provider::gdrive::print_gdrive_config;
-use provider::local::LocalSource;
-use provider::sftp::SftpSource;
-use provider::sftp::connect_sftp;
-use provider::sftp::print_ssh_config;
 use ssdp::SsdpServer;
-use std::net::{IpAddr, SocketAddr};
-use std::path::PathBuf;
 use std::sync::Arc;
 use std::sync::atomic::Ordering;
 use tracing::{error, info};
 use uuid::Uuid;
-
-#[derive(Parser, Debug)]
-#[command(name = "remote-media-pipe")]
-#[command(about = "A non-root remote media streaming pipe")]
-pub struct Cli {
-    #[command(subcommand)]
-    pub provider: ProviderSubcommand,
-}
-
-#[derive(Subcommand, Debug)]
-pub enum ProviderSubcommand {
-    /// local directory path (e.g. ~/Movies or /data/video)
-    Local {
-        source: PathBuf,
-        target: SocketAddr,
-        #[arg(long = "allow", value_name="IP address", num_args = 1..)]
-        allowed: Vec<IpAddr>,
-    },
-    /// remote target (e.g. s7:~/Movies or s7:/var/media)
-    Sftp {
-        source: String,
-        target: SocketAddr,
-        #[arg(long = "allow", value_name="IP address", num_args = 1..)]
-        allowed: Vec<IpAddr>,
-    },
-    /// remote folder path or ID (e.g. qsc:DLNA/)
-    Gdrive {
-        source: String,
-        target: SocketAddr,
-        #[arg(long = "allow", value_name="IP address", num_args = 1..)]
-        allowed: Vec<IpAddr>,
-    },
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -60,24 +18,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     init_logging();
 
     // parse args
-    let cli = Cli::parse();
-    let (source, target, allowed): (Arc<dyn MediaSource>, SocketAddr, Vec<IpAddr>) = match cli.provider {
-        ProviderSubcommand::Local { source, target, allowed } => {
-            //
-            (Arc::new(LocalSource::new(source)), target, allowed)
-        }
-        ProviderSubcommand::Sftp { source, target, allowed } => {
-            let host = print_ssh_config(&source)?;
-            let session = connect_sftp(&host).await?;
-            //
-            (Arc::new(SftpSource::new(session, host.remote_path)), target, allowed)
-        }
-        ProviderSubcommand::Gdrive { source, target, allowed } => {
-            let host = print_gdrive_config(&source)?;
-            //
-            (Arc::new(GDriveSource::new(host)), target, allowed)
-        }
-    };
+    let (source, target, allowed) = parse_and_resolve_args().await?;
     info!("Starting server bound to {}", target);
     info!("Additional allowed IP addresses: {:?}", allowed);
 
